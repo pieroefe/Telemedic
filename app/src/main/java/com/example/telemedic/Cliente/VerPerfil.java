c5ddc208742b590844341e511ed4b42c52d653b4
pieroefe <a20180524@pucp.edu.pe>
2022-12-11 19:12:33 -0500

    final

--- app/src/main/java/com/example/telemedic/Cliente/VerPerfil.java
+++ app/src/main/java/com/example/telemedic/Cliente/VerPerfil.java
@@ -28,6 +28,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.example.telemedic.R;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -40,6 +41,7 @@ import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
+
 import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -51,9 +53,10 @@ import java.util.Map;
 
 public class VerPerfil extends AppCompatActivity {
 
-    Button btn_actualizar_perfil,btn_cancelar,btn_foto_perfil,btn_remover;
-    TextView correo,codigo;
-    EditText nombre;
+    Button btn_actualizar_perfil,btn_cancelar,btn_foto_perfil,btn_foto_perfil2,btn_remover;
+    TextView correo;// = (TextView) findViewById(R.id.tv_correo_perfil);
+    TextView codigo;//=  (TextView) findViewById(R.id.tv_codigo_perfil);
+    EditText nombre;//=(EditText) findViewById(R.id.et_nombre_perfil_usuario);
     ImageView foto;
     private FirebaseFirestore firebaseFirestore;
     StorageReference storageReference;
@@ -62,6 +65,7 @@ public class VerPerfil extends AppCompatActivity {
 
     private static final int COD_SEL_STORAGE = 200;
     private static final int COD_SEL_IMAGE = 300;
+    private Uri image_url;
 
     String photo = "photo";
     String idd;
@@ -69,7 +73,13 @@ public class VerPerfil extends AppCompatActivity {
     File sdImageMainDirectory;
     Uri outputFileUri;
 
+    Uri urifinal;
+
     String imageFilePath;
+
+    boolean actualizar_foto = false;
+
+    //boolean actualizar_foto;
 
 
 
@@ -79,7 +89,7 @@ public class VerPerfil extends AppCompatActivity {
     private File createImageFile() throws IOException{
 
         File stdir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File file = File.createTempFile("fototelemedic",".jpg",stdir);
+        File file = File.createTempFile("fototelemedic",".jpeg",stdir);
         imageFilePath = file.getAbsolutePath();
         return file;
     }
@@ -109,35 +119,106 @@ public class VerPerfil extends AppCompatActivity {
         storageReference = FirebaseStorage.getInstance().getReference();
         firebaseAuth = FirebaseAuth.getInstance();
 
-        correo = findViewById(R.id.tv_correo_perfil);
-        codigo = findViewById(R.id.tv_codigo_perfil);
-        nombre = findViewById(R.id.et_nombre_perfil_usuario);
+        correo = (TextView) findViewById(R.id.tv_correo_perfil);
+        codigo = (TextView) findViewById(R.id.tv_codigo_perfil);
+        nombre = (EditText) findViewById(R.id.et_nombre_perfil_usuario);
         foto = findViewById(R.id.iv_perfil);
 
         btn_actualizar_perfil = findViewById(R.id.btn_editar_perfil);
         btn_cancelar = findViewById(R.id.btn_cancelar_perfil);
         btn_foto_perfil = findViewById(R.id.btn_foto_perfil);
+        btn_foto_perfil2 = findViewById(R.id.btn_foto_perfil2);
 
         progressDialog = new ProgressDialog(this);
 
 
-        getPerfil(idd);
-
-        String correoUser = correo.getText().toString().trim();
-        String codigoUser = codigo.getText().toString().trim();
-        String nombreUser = nombre.getText().toString().trim();
 
 
+        btn_foto_perfil2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadPhoto();
 
 
-        if(correoUser.isEmpty() || codigoUser.isEmpty() || nombreUser.isEmpty() ){
-            Toast.makeText(VerPerfil.this,"No hay datos para presentar", Toast.LENGTH_SHORT).show();
 
-        }else{
 
-            getPerfil(idd);
+                //progressDialog.dismiss();
+            }
+        });
 
-        }
+
+
+
+
+
+
+
+
+        //getPerfil(idd);
+
+        firebaseFirestore.collection("user").document(idd).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+
+                String correoPerfil = documentSnapshot.getString("correo");
+                String codigoPerfil = documentSnapshot.getString("codigo");
+                String nombrePerfil = documentSnapshot.getString("nombre");
+                String fotoPerfil = documentSnapshot.getString("foto");
+                System.out.println("fotoperfil:"+ fotoPerfil);
+
+                correo.setText(correoPerfil);
+                codigo.setText(codigoPerfil);
+                nombre.setText(nombrePerfil);
+
+                String correoUser = ((TextView) findViewById(R.id.tv_correo_perfil)).getText().toString().trim();
+                String codigoUser = codigo.getText().toString().trim();
+                String nombreUser = nombre.getText().toString().trim();
+
+                System.out.println("prueba datos - correo " + correoUser + "codigo " + codigoUser + "nombre " + nombreUser);
+
+
+
+
+                if(correoUser.isEmpty() || codigoUser.isEmpty() || nombreUser.isEmpty() ){
+                    Toast.makeText(VerPerfil.this,"No hay datos para presentar", Toast.LENGTH_SHORT).show();
+
+                }else{
+
+                    //getPerfil(idd);
+
+                }
+
+                try{
+                    if(!fotoPerfil.equals("")){
+
+                        Toast.makeText(getApplicationContext(),"Cargando foto",Toast.LENGTH_SHORT).show();
+                        //Glide.with(VerPerfil.this).load(fotoPerfil).into(foto);
+                        try {
+                            Picasso.with(VerPerfil.this).load(fotoPerfil).resize(150,150).into(foto);
+
+                        }catch (Exception e){
+                            e.printStackTrace();
+                        }
+
+
+                    }
+                }catch (Exception e){
+                    Log.v("Error","e: "+e);
+                }
+
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"Error al obtener los datos", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+
 
 
 
@@ -215,6 +296,10 @@ public class VerPerfil extends AppCompatActivity {
 
 
 
+
+
+
+
     }
 
 
@@ -223,9 +308,17 @@ public class VerPerfil extends AppCompatActivity {
 
     private void UpdatePerfil(String nombreUser,String fotoUser, String id) {
 
+        System.out.println(actualizar_foto);
+
+        String rute_storage_photo = storage_path + "" + foto + "" + firebaseAuth.getUid() +""+idd;
+        StorageReference reference = storageReference.child(rute_storage_photo);
+
         Map<String, Object> map = new HashMap<>();
         map.put("nombre", nombreUser);
-        map.put("foto",fotoUser);
+        //map.put("foto",fotoUser);
+
+        progressDialog.setMessage("Actualizando datos");
+        progressDialog.show();
 
 
 
@@ -237,7 +330,7 @@ public class VerPerfil extends AppCompatActivity {
             @Override
             public void onSuccess(Void unused) {
                 Toast.makeText(getApplicationContext(),"Exito al editar", Toast.LENGTH_SHORT).show();
-                finish();
+
 
             }
         }).addOnFailureListener(new OnFailureListener() {
@@ -248,9 +341,74 @@ public class VerPerfil extends AppCompatActivity {
             }
         });
 
+        if(actualizar_foto == true){
+            actualizar_foto = false;
+            reference.putFile(urifinal).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    System.out.println("Test success: "+ urifinal);
+                    Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while (!uriTask.isSuccessful()){
+                        System.out.println("Test while sucess" + urifinal);
+                        if(uriTask.isSuccessful()){
+                            uriTask.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    System.out.println("onsuccess-uri " + uri);
+
+                                    HashMap<String,Object> map = new HashMap<>();
+                                    map.put("foto",uri);
+                                    firebaseFirestore.collection("user").document(idd).update(map);
+                                    Toast.makeText(getApplicationContext(),"Foto Actulizada", Toast.LENGTH_SHORT).show();
+                                    progressDialog.dismiss();
+                                    finish();
+
+
+
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+
+                                    e.printStackTrace();
+                                    progressDialog.dismiss();
+                                    finish();
+                                }
+                            });
+
+                        }
+
+
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Toast.makeText(VerPerfil.this,"Error al cargar foto", Toast.LENGTH_SHORT).show();
+                    e.printStackTrace();
+                    progressDialog.dismiss();
+                    finish();
+                }
+            });
+        }else{
+            progressDialog.dismiss();
+            finish();
+        }
+
+        //aqui
+
+
+
+
+
+
+
 
 
     }
+
+
+
 
     private void getPerfil(String id) {
         firebaseFirestore.collection("user").document(id).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
@@ -292,15 +450,36 @@ public class VerPerfil extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
-        super.onActivityResult(requestCode,resultCode,data);
+        //super.onActivityResult(requestCode,resultCode,data);
         String rute_storage_photo = storage_path + "" + foto + "" + firebaseAuth.getUid() +""+idd;
         StorageReference reference = storageReference.child(rute_storage_photo);
 
 
+
+        Log.d("image_url","requestCode - RESULT_OK" + requestCode + "" + RESULT_OK);
+        if(resultCode == RESULT_OK){
+            if(requestCode == COD_SEL_IMAGE){
+                actualizar_foto = true;
+                urifinal = data.getData();
+                Picasso.with(VerPerfil.this).load(urifinal).resize(150,150).into(foto);
+                //subirfoto(urifinal);
+
+
+
+            }
+        }
+
+
+
+
+
         if(requestCode == CAMERA_ACTION_CODE && resultCode == RESULT_OK ) {
 
-            progressDialog.setMessage("Actualizando foto");
-            progressDialog.show();
+            actualizar_foto = true;
+
+
+
+
 
             /*
 
@@ -322,12 +501,14 @@ public class VerPerfil extends AppCompatActivity {
 
 
             //Uri uri = FileProvider.getUriForFile(this, VerPerfil.this.getPackageName()+".provider", file);
-            Uri uri = Uri.fromFile(new File(imageFilePath));
+            urifinal = Uri.fromFile(new File(imageFilePath));
+
+
 
 
 
             System.out.println("---AQUI HAY PRUEBAS---");
-            System.out.println("esto es la foto" + uri);
+            System.out.println("esto es la foto" + urifinal);
 
 
 
@@ -339,54 +520,21 @@ public class VerPerfil extends AppCompatActivity {
             //System.out.println(path);
 
             //Uri image_url = Uri.parse(path);
-            //foto.setImageURI(image_url);
+            //foto.setImageURI(urifinal);
+            Picasso.with(VerPerfil.this).load(urifinal).resize(150,150).into(foto);
+
+
+
+
+            System.out.println("esto es la uri de la foto " + urifinal);
 
 
 
 
 
-
-
-
-            System.out.println("esto es la uri de la foto " + uri);
-
-
-
-
-            reference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                @Override
-                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                    Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
-                    while (!uriTask.isSuccessful()){
-                        if(uriTask.isSuccessful()){
-                            uriTask.addOnSuccessListener(new OnSuccessListener<Uri>() {
-                                @Override
-                                public void onSuccess(Uri uri) {
-                                    System.out.println("onsuccess-uri " + uri);
-
-                                    HashMap<String,Object> map = new HashMap<>();
-                                    map.put("foto",uri);
-                                    firebaseFirestore.collection("user").document(idd).update(map);
-                                    Toast.makeText(getApplicationContext(),"Foto Actulizada", Toast.LENGTH_SHORT).show();
-                                    progressDialog.dismiss();
-
-
-                                }
-                            });
-
-                        }
-
-
-                    }
-                }
-            }).addOnFailureListener(new OnFailureListener() {
-                @Override
-                public void onFailure(@NonNull Exception e) {
-                    Toast.makeText(VerPerfil.this,"Error al cargar foto", Toast.LENGTH_SHORT).show();
-                }
-            });
 
         }
+        super.onActivityResult(requestCode,resultCode,data);
 
 
 
@@ -399,6 +547,59 @@ public class VerPerfil extends AppCompatActivity {
 
 
     }
+
+
+
+    private void subirfoto(Uri image_url){
+        progressDialog.setMessage("Actualizando foto");
+        progressDialog.show();
+        String route_storage_photo = storage_path + "" + photo +"" + firebaseAuth.getUid() + "" + idd;
+        StorageReference reference = storageReference.child(route_storage_photo);
+        reference.putFile(image_url).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Task<Uri> uriTask = taskSnapshot.getStorage().getDownloadUrl();
+                while(!uriTask.isSuccessful());
+                if(uriTask.isSuccessful()){
+                    uriTask.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            String download_uri = uri.toString();
+                            HashMap <String, Object> map = new HashMap<>();
+                            map.put("foto",download_uri);
+                            firebaseFirestore.collection("user").document(idd).update(map);
+                            Toast.makeText(VerPerfil.this,"Foto actualizada",Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+
+                        }
+                    });
+                }
+
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        });
+
+    }
+
+
+
+    private void uploadPhoto(){
+        Intent i = new Intent(Intent.ACTION_PICK);
+        i.setType("image/*");
+
+        startActivityForResult(i,COD_SEL_IMAGE);
+
+    }
+
+
+
+
+
+
 
 
 
---
+++ app/src/main/java/com/example/telemedic/Login/GlideConf.java
@@ -0,0 +1,23 @@
+package com.example.telemedic.Login;
+
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.Registry;
+import com.bumptech.glide.module.AppGlideModule;
+import com.google.firebase.storage.StorageReference;
+import com.firebase.ui.storage.images.FirebaseImageLoader;
+
+import java.io.InputStream;
+
+
+public class GlideConf extends AppGlideModule {
+
+    @Override
+    public void registerComponents(@NonNull Context context, @NonNull Glide glide, @NonNull Registry registry) {
+        registry.append(StorageReference.class, InputStream.class, new FirebaseImageLoader.Factory());
+    }
+}
